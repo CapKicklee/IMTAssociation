7673c4bbebe4b938e23ee6c230bc7905904e7858
Juliette FRETAY <fretay.e1401371@etud.univ-ubs.fr>
2017-11-03 10:51:07 +0100

    Modification Test Persistence Pays Article Adherent et Adresse

--- src/test/java/database/persistence/TestPersistence.java
+++ src/test/java/database/persistence/TestPersistence.java
@@ -1,23 +1,29 @@
 package database.persistence;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 import database.services.AdherentPersistenceJPA;
+import database.services.AdressePersistenceJPA;
+import database.services.ArticlePersistenceJPA;
+import database.services.PaysPersistenceJPA;
 
 public class TestPersistence {
 
 	public static void main(String[] args) {
-		/*
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistence-unit1");
-		EntityManager em = emf.createEntityManager();
-		System.out.println(em.isOpen());
-		em.close();
-		emf.close();*/
 		
 		AdherentPersistenceJPA jpa = new AdherentPersistenceJPA();
+		System.out.println(jpa.loadAll().toString());
 		System.out.println(jpa.countAll());
+		
+		ArticlePersistenceJPA jpa2 = new ArticlePersistenceJPA();
+		System.out.println(jpa2.loadAll().toString());
+		System.out.println(jpa2.countAll());
+		
+		AdressePersistenceJPA jpa3 = new AdressePersistenceJPA();
+		System.out.println(jpa3.loadAll().toString());
+		System.out.println(jpa3.countAll());
+		
+		PaysPersistenceJPA jpa4 = new PaysPersistenceJPA();
+		System.out.println(jpa4.loadAll().toString());
+		System.out.println(jpa4.countAll());
 	}
 
 }
